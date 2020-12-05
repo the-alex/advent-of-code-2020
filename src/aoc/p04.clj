4cac3135fee8a737265e62950a5c4173e10a845e
Alex Chojnacki <thealex@umich.edu>
2020-12-04 22:52:02 -0500

    p4 done, but 153 is not the answer for my input
    at least, according to this message I got:
    
    > That's not the right answer. Curiously, it's the right answer for
    > someone else; you might be logged in to the wrong account or just
    > unlucky. In any case, you need to be using your puzzle input. If
    > you're stuck, make sure you're using the full input data; there are
    > also some general tips on the about page, or you can ask for hints on
    > the subreddit. Because you have guessed incorrectly 5 times on this
    > puzzle, please wait 5 minutes before trying again. (You guessed 153.)
    > [Return to Day 4]

--- src/aoc/p04.clj
+++ src/aoc/p04.clj
@@ -2,100 +2,142 @@
   (:require [clojure.string :as str]
             [clojure.set :refer [difference]]))
 
-(def required-kw #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"})
-
-(def test-input
-  "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
-
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
-
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
-
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in")
-
 (defn parse-input [string-content]
   (-> string-content
       str/split-lines))
 
-(defn read-strings-until-newline [lines]
+(defn next-passport-string [lines]
   "Given a list of strings, intended to be from str/split, read the collection
   until we find a newline. This newline indicates the end of one section of
   passport info."
   (loop [[this-line & remaining-lines] lines
          record-strings []]
     (if (empty? this-line)
-      {:record-line (str/join " " record-strings) :remaining-lines remaining-lines}
+      [(str/join " " record-strings) remaining-lines]
       (recur
        remaining-lines
        (conj record-strings this-line)))))
 
+(defn line->passport [line]
+  (let [string-pairs (map #(str/split % #":") (str/split line #" "))]
+    (into {} string-pairs)))
 
-(defn line->kw-set [line]
-  (let [string-pairs (map #(str/split % #":") (str/split line #" "))
-        present-keys (set (map first string-pairs))]
-    present-keys))
-
-(defn lines->kw-sets [lines]
+(defn lines->passports [lines]
   (loop [remaining-lines lines
-         kw-sets []]
+         passports []]
     (if (empty? remaining-lines)
-      kw-sets
-      (let [{record-line :record-line
-             remaining-lines :remaining-lines}
-            (read-strings-until-newline remaining-lines)
-            kw-set (line->kw-set record-line)]
+      passports
+      (let [[record-line remaining-lines] (next-passport-string remaining-lines)
+            passport (line->passport record-line)]
         (recur
          remaining-lines
-         (cons kw-set kw-sets))))))
+         (cons passport passports))))))
 
-(defn valid-kw-set? [kw-set]
-  (let [set-diff (difference required-kw kw-set)]
-    ;; Set diff is empty -- all eight items present
-    (or (empty? set-diff)
-        ;; OR the only item is "cid" -- give a pass
-        (not (empty? (difference #{"cid"} set-diff))))))
 
-(defn count-valid-records [kw-sets]
-  (->> kw-sets
-       (map valid-kw-set?)
-       (filter true?)
+;; A passport is valid if it contains all the required keywords. Howver, if
+;; missing only `cid`, then we'll count it as just fine.
+(defn p1-is-valid? [passport]
+  (let [required-keys #{"hgt" "pid" "byr" "eyr" "iyr" "ecl" "cid" "hcl"}
+        present-keys (keys passport)
+        missing-keys (difference required-keys present-keys)]
+    ;; if there are no missing keys OR the only missing key is CID
+    (or (empty? missing-keys) (= missing-keys #{"cid"}))))
+
+
+(defn p1 [input-filepath]
+  (->> input-filepath
+       slurp
+       str/split-lines
+       lines->passports
+       (filter p1-is-valid?)
+       count))
+
+
+(def validators
+  {;; byr (Birth Year) - four digits; at least 1920 and at most 2002.
+   "byr" (fn [value] (let [parsed-int (read-string value)] (<= 1920 parsed-int 2002)))
+
+   ;; iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+   "iyr" (fn [value] (let [parsed-int (read-string value)] (<= 2010 parsed-int 2020)))
+
+   ;; eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+   "eyr" (fn [value] (let [parsed-int (read-string value)] (<= 2020 parsed-int 2030)))
+
+   ;; hgt (Height) - a number followed by either cm or in:
+   ;; If cm, the number must be at least 150 and at most 193.
+   ;; If in, the number must be at least 59 and at most 76.
+   "hgt" (fn [value] (when-let
+                      [matched-components (->> value (re-matches #"(\d+)([a-z]+)"))]
+                       (let [[_ magnitude unit] matched-components
+                             magnitude        (read-string magnitude)]
+                         (cond
+                           (= unit "cm") (<= 150 magnitude 193)
+                           (= unit "in") (<= 59 magnitude 76)
+                           :else false))))
+
+   ;; hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+   "hcl" (fn [value] (->> value (re-find #"^#[a-z0-9]{6}$") nil? not))
+
+   ;; ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+   "ecl" (fn [value] (let [enums #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"}]
+                       (contains? enums value)))
+
+   ;; pid (Passport ID) - a nine-digit number, including leading zeroes.
+   "pid" (fn [value] (->> value (re-find #"^[0-9]{9}$") nil? not))
+
+   ;; cid (Country ID) - ignored, missing or not.
+   "cid" (fn [value] true)})
+
+(comment
+  (let [is-valid? (get validators "hgt")]
+    (is-valid? "150cm")))
+
+(defn p2-is-valid? [passport]
+  (let [validation-results
+        (for [field (keys validators)
+              :let [is-valid? (get validators field)
+                    value (get passport field)]
+              :when (contains? passport field)]
+          (is-valid? value))]
+    (and
+     ;; Every statement is true -- AND
+     (every? true? validation-results)
+     ;; there are at least seven keys found
+     (<= 7 (count validation-results)))))
+
+(defn p2 [input-filepath]
+  (->> input-filepath
+       slurp
+       str/split-lines
+       lines->passports
+       (filter p2-is-valid?)
        count))
 
 (comment
 
-  (let [lines (str/split-lines test-input)]
-    (count-valid-records (lines->kw-sets lines))
-    #_(map count (lines->kw-sets lines))
-    #_(lines->kw-sets lines))
+  (let [valid-passports (slurp "data/input-p04-valid-passports.txt")]
 
-  (let [lines (str/split-lines (slurp "data/input-p04-1.txt"))]
-    #_(count-valid-records (lines->kw-sets lines))
-    (last lines)
-    )
+    (p1 "data/input-p04-1.txt")
+    ;; => 222
+    (p2 "data/input-p04-1.txt")
+    ;; => 153
 
+    (p2 "data/input-p04-invalid-passports.txt")
+    ;; => 0
+    (p2 "data/input-p04-valid-passports.txt")
+    ;; => 4
 
-  ;; 1 has all eight keys -- valid!
-  (difference required-kw #{"hgt" "pid" "byr" "eyr" "iyr" "ecl" "cid" "hcl"})
-  (valid-kw-set? #{"hgt" "pid" "byr" "eyr" "iyr" "ecl" "cid" "hcl"})
+    (->> valid-passports
+         str/split-lines
+         lines->passports
+         (filter p2-is-valid?)
+         count)
 
-  ;; 2 is missing "hgt" -- invalid!
-  (difference required-kw #{"pid" "byr" "eyr" "iyr" "ecl" "cid" "hcl"})
-  (valid-kw-set? #{"pid" "byr" "eyr" "iyr" "ecl" "cid" "hcl"})
+    (let [line "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+          passport (line->passport line)]
+      (let [present-keys (set (keys passport))] present-keys))
 
-  ;; 3 missing cid, but we give it a pass -- valid!
-  (difference required-kw #{"hgt" "pid" "byr" "eyr" "iyr" "ecl" "hcl"})
-  (valid-kw-set? #{"hgt" "pid" "byr" "eyr" "iyr" "ecl" "hcl"})
+    (let [lines (str/split-lines (slurp "data/input-p04-1.txt"))]
+      (lines->passports lines))
 
-  ;; NOTE Was `true`, now `false` issue was logical -- needed to wrap `empty?`
-  ;; with `not`
-  ;; 4 is missing cid and byr, so set diff is larger than cid -- invalid!
-  (difference required-kw #{"hgt" "pid" "eyr" "iyr" "ecl" "hcl"})
-  (valid-kw-set? #{"hgt" "pid" "eyr" "iyr" "ecl" "hcl"})
-
-  (read-strings-until-newline (take 10 (str/split-lines test-input))))
+    (next-passport-string (take 5 (str/split-lines test-input)))))
