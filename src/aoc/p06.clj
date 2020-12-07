83fc586c244951359f0b1737e0b930694b0b7c0b
Alex Chojnacki <thealex@umich.edu>
2020-12-06 21:30:21 -0500

    clean up day 6 a little

--- src/aoc/p06.clj
+++ src/aoc/p06.clj
@@ -7,15 +7,16 @@
        (map str/split-lines)
        (map #(map set %))))
 
+(defn reduce-and-count [groups set-op]
+  (reduce + (map count (map #(reduce set-op %) groups))))
+
 (defn p1 [input-filename]
-  (let [groups            (-> input-filename slurp read-groups)
-        all-answers-per-group (map #(reduce set/union %) groups)]
-    (reduce + (map count all-answers-per-group))))
+  (let [groups (-> input-filename slurp read-groups)]
+    (reduce-and-count groups set/union)))
 
 (defn p2 [input-filename]
-  (let [groups            (-> input-filename slurp read-groups)
-        consensus-answers-per-group (map #(reduce set/intersection %) groups)]
-    (reduce + (map count consensus-answers-per-group))))
+  (let [groups (-> input-filename slurp read-groups)]
+    (reduce-and-count groups set/intersection)))
 
 (comment
   (p1 "data/input-p06.txt")
